22e02f9fb58b658dc61f18f384d36fa65fbf32d5
benmaddison <benm@workonline.africa>
2022-08-12 12:49:21 +0000

    Deploying to gh-pages from @ job/draft-rpki-checklists@62d5ec5c01424ed1265f4b4eaacbc551098ad111 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.2.dev30',
+    VERSION: '0.8.2.dev33',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
